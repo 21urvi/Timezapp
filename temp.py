144f0719f9c8c00ee1f5d7f13d4ac71284f02c05
21urvi <u7979137@gmail.com>
2024-12-22 12:45:29 +0530

    Update

--- temp.py
+++ temp.py
@@ -2,8 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Route for the homepage (index.html)
 @app.route('/')
+def welcome():
+    return render_template('welcome.html')
+
+# Route for the homepage (index.html)
+@app.route('/index.html')
 def first():
     return render_template('index.html')
 
@@ -11,6 +15,8 @@ def first():
 @app.route('/landing.html')
 def landing():
     return render_template('landing.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
